b9fae1c5322757c9e8d79e877b3900a181fc7c12
Justin Favaloro <justin_f_111@hotmail.com>
2020-08-06 08:07:12 +1000

    Initial Commit

---
+++ app/Sale.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sale extends Model
+{
+    public function customer() {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function product() {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function employee() {
+        return $this->belongsTo(Employee::class);
+    }
+}
---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Employee;
+use App\Product;
+use App\Sale;
+use App\Customer;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+    }
+}
